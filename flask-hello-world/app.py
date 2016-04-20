7806beb4241c5451355781f93a847548022d338f
nexttrack <daf@schommerce.com>
2016-04-19 21:11:06 -0500

    Added debugger

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,6 +6,8 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
@@ -30,6 +32,13 @@ def float_type(value):
 def path_type(value):
     print value
     return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "faadhil":
+        return "Hello, {}".format(name), 200
+    else:
+        return "not found", 404
 
 # start the development server using the run() method
 if __name__ =="__main__":
